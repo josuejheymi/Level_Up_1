f7116d5ebf8142cb8698ce263244a23e1a94c75c
JyTO <jo.ticona@duocuc.cl>
2025-10-27 14:58:32 -0300

    Se agrego los componentes para visualizar los productos y el arreglo para que funcione con localStorage , tambien un peueño panel de admin para agregar productos

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,14 @@
-export default function Home() {
+import ProductList from "../Components/ProductList";
+
+export default function Home({ products }) {
     return (
         <section className="container my-5">
             <h1>Bienvenido</h1>
             <p>Tienda online de videojuegos, consolas, pc gamers y más . . .</p>
-            
+        <div>
+            <h1>Productos en venta</h1>
+            <ProductList products={products} />
+        </div>
         </section>
     );
 }
